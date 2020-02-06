560df9af95c70ecc0e9813c7d568f5aea5b57b9d
lTheGrifterl <48074427+lTheGrifterl@users.noreply.github.com>
2020-02-06 17:03:25 -0500

    Update AnotherTokenizer.java

--- AnotherTokenizer.java
+++ AnotherTokenizer.java
@@ -15,12 +15,16 @@ import java.io.*;
      FileReader fr = new FileReader("test1.lua");
       BufferedReader br = new BufferedReader(fr);
       StreamTokenizer streamtokenizer = new StreamTokenizer(br);
-      List<String> tokens = new ArrayList<String>();
-
+      int m = 10;
+      int n = 20;
+      String[][] tokens = new String[m][n];
       streamtokenizer.eolIsSignificant(true);
   
       String token = null;
+      // column(array number) position
       int i = 0;
+      // row position
+      int j = 0;
       int t;
       while((t = streamtokenizer.nextToken()) != StreamTokenizer.TT_EOF)
       {
@@ -28,31 +32,40 @@ import java.io.*;
       {
         //end of line
         case StreamTokenizer.TT_EOL: 
-                System.out.println(" "); 
+                System.out.println(" ");
+          //moves to next array (column)
+                i ++;
+          //resets the array position back to 0 (rows)
+                j = 0;
                 break; 
             //print words    
        case StreamTokenizer.TT_WORD: 
                 System.out.print(streamtokenizer.sval + " "); 
                 token = streamtokenizer.sval;
-                tokens.add(token);
+                tokens[i][j] = token;
+                j++;
                 break;
               //print numbers
         case StreamTokenizer.TT_NUMBER:
                System.out.print(streamtokenizer.nval + " ");
                token = streamtokenizer.sval;
-                tokens.add(token);
+               tokens[i][j] = token;
+                j++;
                break;
             //prints everything inside quotes   
-         case '"':
+        case '"':
                token = String.valueOf((char)t);
-               tokens.add(token);
+               tokens[i][j] = token;
+               j++;
                System.out.print((char) t);
                System.out.print(streamtokenizer.sval);
                token = streamtokenizer.sval;
-               tokens.add(token);
+               tokens[i][j] = token;
+               j++;
                token = String.valueOf((char)t);
                System.out.print((char) t);
-               tokens.add(token);
+               tokens[i][j] = token;
+               j++;
                break;
                //ignores bracket
         case '{':
@@ -65,22 +78,28 @@ import java.io.*;
         default:
                     System.out.print((char) t );
                     token = String.valueOf((char)t);
-                    tokens.add(token);
+                    tokens[i][j] = token;
+                    j++;
+                    
         
       } 
 
 
       }
-      //prints out the arraylist
-      for (int k= 0; k<tokens.size();k++)
-      {
-        //prints nothing if null
-        if (tokens.get(k) == null)
-         System.out.print(""); 
-       else
-       System.out.println(tokens.get(k)); 
-        }
-      
+      //goes down the columns and prints each array in order, does not print nulls
+      System.out.println(" ");
+      int a, b;
+      for ( a = 0; a < m; a++){
+        if(tokens[a] != null)
+        for(b = 0; b < n; b++){
+          if(tokens[a][b] != null)
+      System.out.print(tokens[a][b] + " ");
+      else
+        b++;
+          
   }
-  
+        System.out.println(" ");
+      
+      }
 }
+  }
